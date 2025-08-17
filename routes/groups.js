2569898d1001936a9a966550c6e73cc59bed06f3
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-17 22:05:33 +0000

    Create groups.js

---
+++ routes/groups.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+const { 
+  getGroups, 
+  createGroup, 
+  getGroup, 
+  joinGroup,
+  getGroupMessages,
+  sendGroupMessage
+} = require('../controllers/groupController');
+
+router.route('/')
+  .get(getGroups)
+  .post(protect, createGroup);
+
+router.route('/:id')
+  .get(getGroup);
+
+router.route('/:id/join')
+  .post(protect, joinGroup);
+
+router.route('/:id/messages')
+  .get(protect, getGroupMessages)
+  .post(protect, sendGroupMessage);
+
+module.exports = router;
