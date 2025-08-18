e368005a192615e4e3fbaa998a58467d6fa4fdd2
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-18 01:18:54 +0000

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const questionController = require('../controllers/questionController');
+const { 
+  getQuestions, 
+  getQuestion, 
+  createQuestion,
+  updateQuestion, 
+  deleteQuestion, 
+  likeQuestion, 
+  createAnswer 
+} = require('../controllers/questionController');
 
 router.route('/')
-  .get(questionController.getQuestions)
-  .post(protect, questionController.createQuestion);
+  .get(getQuestions)
+  .post(protect, createQuestion);
 
 router.route('/:id')
-  .get(questionController.getQuestion)
-  .put(protect, questionController.updateQuestion)
-  .delete(protect, questionController.deleteQuestion);
+  .get(getQuestion)
+  .put(protect, updateQuestion)
+  .delete(protect, deleteQuestion);
 
 router.route('/:id/like')
-  .post(protect, questionController.likeQuestion);
+  .post(protect, likeQuestion);
 
 router.route('/:id/answers')
-  .post(protect, questionController.createAnswer);
+  .post(protect, createAnswer);
 
 module.exports = router;
