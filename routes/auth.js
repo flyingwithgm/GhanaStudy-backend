718a9353042b1578dcb0644782fa86d6abeba4f4
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-18 00:54:15 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const { register, login, getMe } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/me', authController.getMe);
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
